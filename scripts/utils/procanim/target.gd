751a6b2523378d65077bd8cb0d2b72a7d0e7972f
Popcorn <vihaanraina00@gmail.com>
2026-01-07 21:10:18 +0530

    Create abomination

--- scripts/utils/procanim/target.gd
+++ scripts/utils/procanim/target.gd
@@ -1,51 +1,23 @@
-@tool
 extends Marker3D
 
-### this code is implemented from Crigz vs Game Dev's guide
-
-@export var step_target : Node3D
+@export var step_target: Node3D
 @export var step_distance: float = 0.5
-@export var adjacent_target : Node3D
-@export var player : CharacterBody3D
-@export var leg : Node3D
 
-var is_stepping : bool = false
+@export var adjacent_target: Node3D
 
-func _process(delta: float) -> void:
-	if (!is_stepping and !adjacent_target.is_stepping and abs(global_position.distance_to(step_target.global_position)) > step_distance):
+var is_stepping := false
+
+func _process(delta):
+	if !is_stepping && !adjacent_target.is_stepping && abs(global_position.distance_to(step_target.global_position)) > step_distance:
 		step()
 
 func step():
-	#is_stepping = true
-	#var target_position = step_target.global_position
-	#var half_way = (global_position + step_target.global_position) / 2
-	#var prevlegy = -0.714 ## don't ask why this is a variable
-	#var step_tween2 = get_tree().create_tween()
-	#
-	#step_tween2.tween_property(leg.mesh, "position:z", prevlegy + 1, 0.1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	#
-	#step_tween2.tween_property(self, "global_position", target_position + player.arji * 0.4, 0.1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	#step_tween2.tween_property(leg.mesh,"position:z", prevlegy, 0.1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
-	#
-	#step_tween2 = get_tree().create_tween()
-	#
-	#is_stepping = false
-	#await get_tree.create_timer(0.3).timeou
 	var target_pos = step_target.global_position
 	var half_way = (global_position + step_target.global_position) / 2
-	var prevlegy = -0.714
 	is_stepping = true
 	
-	
+	print(str(get_parent().name))
 	var t = get_tree().create_tween()
-	
-	t.tween_property(leg.mesh, "position:z", prevlegy + 1, 0.1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
-	t.tween_property(self, "global_position", target_pos + (Vector3(player.arji.x * 0.3,0,player.arji.z * 0.3)), 0.1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
-	t.tween_callback(func():
-		var t2 = get_tree().create_tween()
-		t2.tween_property(leg.mesh, "position:z", prevlegy, 0.1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
-		t2.tween_callback(func(): is_stepping = false)
-	)
-	
-	
-	
+	t.tween_property(self, "global_position", half_way + (get_parent().transform.basis.y * 0.75), 0.2).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	t.tween_property(self, "global_position", Vector3(target_pos.x + (get_parent().arji.x * 0.4), target_pos.y, target_pos.z + (get_parent().arji.z * 0.4)) , 0.2).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	t.tween_callback(func(): is_stepping = false)
